7b5dfe4bfae82db7ce7376f7a906461f8595d8b6
MatiasRamsland01 <259150@uis.no>
2021-09-14 22:07:32 +0200

    added sign up page

--- website/auth.py
+++ website/auth.py
@@ -3,10 +3,10 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
   return render_template('signup.html')
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
   return render_template('login.html')
