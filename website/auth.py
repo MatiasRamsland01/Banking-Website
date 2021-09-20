a204c88344a0dd493a82037c170acc17f3685065
Chipok0101 <c.pokhrel@uis.no>
2021-09-20 21:16:07 +0200

    weird .py files. ???

--- website/auth.py
+++ website/auth.py
@@ -1,23 +1,33 @@
 from os import error
 import re
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect
+from flask.helpers import url_for
+from website.forms import RegisterForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-  if request.method == 'POST':
-    name = request.form.get('name')
-    email = request.form.get('email')
-    password1 = request.form.get('password1')
-    password2 = request.form.get('password2')
-    if len(name) < 2:
-      flash('Name needs to be greater than 1 character', category='error')
+  form = RegisterForm()
+  if form.validate_on_submit():
+    firstName = form.nameFirst.data
+    lastName = form.nameLast.data
+    email = form.email.data
+    password1 = form.password1.data
+    password2 = form.password2.data
+    return redirect(url_for('views.home'))
 
-    #Pseudo code: if input not valid --> print error message --> else: add user to database
+  return render_template('signup.html', form=form)  
 
-  return render_template('signup.html')
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template('login.html')
+  form = LoginForm()
+  if form.validate_on_submit():
+    logEmail = form.email.data
+    logPassword = form.password.data
+    return redirect(url_for('views.home'))
+
+  return render_template('login.html', form=form)
