45aca0fe8e4dd2bc8d253ceacf3e36bbff6a8db5
MatiasRamsland01 <259150@uis.no>
2021-10-02 08:35:40 +0200

    Disabled recaptcha for easier testing

--- website/__init__.py
+++ website/__init__.py
@@ -24,6 +24,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'bd5049afa301c7c5d709f821'
     app.config['RECAPTCHA_PUBLIC_KEY'] = '6LeJKpYcAAAAAK9NxeH7cNAPl9BWMQk16hkMdpFy'
     app.config['RECAPTCHA_PRIVATE_KEY'] = '6LeJKpYcAAAAAIK7he7W0f490MZ-t_V_8cDYFDCK'
+    app.config['RECAPTCHA_ENABLED'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
