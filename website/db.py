e1a0ec2aa4399c33407de8ddc64572871d96bcc4
Chillu1 <lukasz20002604@gmail.com>
2021-10-05 12:05:09 +0200

    feat: transaction setup

--- website/db.py
+++ website/db.py
@@ -13,8 +13,6 @@ class User(UserMixin, db.Model):
     username = db.Column( db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=False, nullable=False)
-    # TODO Might be temporary, we probably dont want to store money with user info.
-    money = db.Column(db.String(40), unique=False, nullable=True)  # db.Numeric(16, True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -31,31 +29,17 @@ class User(UserMixin, db.Model):
         if self.password == password:
             return True
 
+class Transaction(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key = True)
+    # Out Id & Money can be null because we might put in money through an ATM
+    out_id = db.Column(db.Integer, nullable = True)#TODO ForeignKey?
+    out_money = db.Column(db.String(40), nullable = True)
+    in_id = db.Column(db.Integer)#TODO ForeignKey?
+    in_money = db.Column(db.String(40))
+    message = db.Column(db.String(120))
+    # TimeStamp?
+    pass
+
 
 def init_db():
     db.create_all()
-
-    # admin = User(username='admin', email='admin@example.com', password='Test123#')
-    # guest = User(username='guest', email='guest@example.com', password='Test123#')
-    # admin.money = "32.123456789101112"
-
-    # db.session.add(admin)
-    # db.session.add(guest)
-    # db.session.commit()
-
-# TEST
-# db.drop_all()
-# db.create_all()
-
-# admin = User(username='admin', email='admin@example.com', password='Test123#')
-# guest = User(username='guest', email='guest@example.com', password='Test123#')
-# admin.money = "32.123456789101112"
-
-# db.session.add(admin)
-# db.session.add(guest)
-# db.session.commit()
-
-# User.query.all()
-# queried_admin = User.query.filter_by(email ='admin@example.com').first()
-# queried_guest = User.query.filter_by(username='guest').first()
-# print(str(queried_admin)+" "+str(queried_admin.id)+" "+str(queried_guest.id)+"_"+str(queried_admin.money))
