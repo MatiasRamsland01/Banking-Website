071247c02cf8323c8e7d1ab1abbdeae1f0b9cd44
MatiasRamsland01 <259150@uis.no>
2021-10-14 17:02:57 +0200

    forgot to remove test code

--- website/views.py
+++ website/views.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template
-from website.db import db
     
 
 views = Blueprint('views', __name__)
@@ -7,6 +6,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    db.drop_all()
-    db.create_all()
     return render_template("home.html")
