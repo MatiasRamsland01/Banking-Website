92021867f333b9838f6a3689e9a8a252163dac6e
MatiasRamsland01 <259150@uis.no>
2021-10-25 20:19:05 +0200

    test postgres

--- website/__init__.py
+++ website/__init__.py
@@ -14,13 +14,18 @@ from flask_limiter.util import get_remote_address
 from flask_talisman import Talisman
 from werkzeug.exceptions import _RetryAfter
 
+
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
-    
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
     csp = {
     'default-src': [
         '\'self\'',
