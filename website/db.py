535778f7ac9c64c873549d501d339e75f175471d
MatiasRamsland01 <259150@uis.no>
2021-10-25 21:45:57 +0200

    postgrest test

--- website/db.py
+++ website/db.py
@@ -11,6 +11,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)  # main.get_app()
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
