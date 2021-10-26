3fda66f1d04622d7e194c52be8a33fd4b3b303fd
MatiasRamsland01 <259150@uis.no>
2021-10-26 09:40:53 +0200

    test heroku

--- website/__init__.py
+++ website/__init__.py
@@ -23,8 +23,7 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
-        uri = uri.replace("postgres://", "postgresql://", 1)
+    uri = uri.replace("postgres://", "postgresql://", 1)
     db = SQLAlchemy(app)
     db.create_all()
 
