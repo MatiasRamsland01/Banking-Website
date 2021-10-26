c79123ef46d22fd466c9eb8736e087886c248733
MatiasRamsland01 <259150@uis.no>
2021-10-26 09:55:51 +0200

    Revert "Revert "test postgres""
    This reverts commit af84277d6a3d62cb233e0829a8bdc0c824dca335.

--- website/__init__.py
+++ website/__init__.py
@@ -22,10 +22,6 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
     csp = {
     'default-src': [
         '\'self\'',
