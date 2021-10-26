1a63a5ef2dab1435884b19ddb25f6f7cf27b0cc5
MatiasRamsland01 <259150@uis.no>
2021-10-26 17:42:39 +0200

    test

--- website/db.py
+++ website/db.py
@@ -16,7 +16,8 @@ Base = declarative_base()
 
 app = Flask(__name__)  # main.get_app()
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
-
+if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 engine = create_engine(uri)
