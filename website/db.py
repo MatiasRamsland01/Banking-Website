8e6ddcccbb6b098c6bbaa87b14af3aef488704b1
MatiasRamsland01 <259150@uis.no>
2021-10-26 09:53:44 +0200

    Revert "test heroku"
    This reverts commit 1379173366f75ce34d39c58fb47090947096811b.

--- website/db.py
+++ website/db.py
@@ -9,14 +9,6 @@ from flask_login import UserMixin
 from sqlalchemy import or_
 from sqlalchemy.sql.expression import null
 from werkzeug.security import generate_password_hash, check_password_hash
-    
-app = Flask(__name__)
-
-uri = os.getenv("DATABASE_URL")  # or other relevant config var
-if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-db = SQLAlchemy(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
