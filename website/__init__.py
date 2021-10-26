88e5c0c33318f8ec74948b896f1a3c029125cdd6
MatiasRamsland01 <259150@uis.no>
2021-10-26 09:53:45 +0200

    Revert "postgrest test"
    This reverts commit b47933cd17be7c0f8817be3e6b99852829530251.

--- website/__init__.py
+++ website/__init__.py
@@ -16,17 +16,12 @@ from werkzeug.exceptions import _RetryAfter
 
 
 
-db = SQLAlchemy()
 login_manager = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
-    uri = os.getenv("DATABASE_URL")  # or other relevant config var
-    if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
-    db = SQLAlchemy(app)
+    
     csp = {
     'default-src': [
         '\'self\'',
@@ -63,8 +58,6 @@ def create_app():
 
     ReCaptcha(app)
     QRcode(app)
-    db.init_app(app)
-    app.cli.add_command(init_db_command)
 
     from .views import views
     from .auth import auth
@@ -98,13 +91,4 @@ def create_app():
 
 
 
-def init_db():
-    db.create_all()
 
-
-@click.command("init-db")
-@with_appcontext
-def init_db_command():
-    """Clear existing data and create new tables."""
-    init_db()
-    click.echo("Initialized the database.")
