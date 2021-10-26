e7868dffa00dcf7de9e9328471f4b494011c74cc
MatiasRamsland01 <259150@uis.no>
2021-10-26 09:20:45 +0200

    test heroku

--- website/__init__.py
+++ website/__init__.py
@@ -40,10 +40,11 @@ def create_app():
     
     csrf = CSRFProtect()
     csrf.init_app(app)
-    db_url = os.environ.get("DATABASE_URL")
-
+    
 
     """
+    db_url = os.environ.get("DATABASE_URL")
+
     if db_url is None:
         # default to a sqlite database in the instance folder
         db_path = os.path.join(app.instance_path, "flaskr.sqlite")
--- website/db.py
+++ website/db.py
@@ -13,8 +13,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
+"""
 if uri.startswith("postgres://"): # from SQLAlchemy 1.14, the uri must start with postgresql, not postgres, which heroku provides
     uri = uri.replace("postgres://", "postgresql://", 1)
+"""
+
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
