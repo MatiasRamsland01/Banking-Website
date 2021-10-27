82b2071e1e39ed1c4996a5f6e31ca8d3dc2e7c3b
MatiasRamsland01 <259150@uis.no>
2021-10-27 10:50:11 +0200

    test

--- website/auth.py
+++ website/auth.py
@@ -14,7 +14,8 @@ from sqlalchemy import literal
 from sqlalchemy.sql.expression import false
 from werkzeug.local import LocalProxy
 from website.db import User, Transaction, EncryptMsg, DecryptMsg
-from website import db, init_db
+from website import db
+from website import init_db
 from flask_wtf.recaptcha.validators import Recaptcha
 from website.forms import RegisterForm, LoginForm, TransactionForm, ATMForm
 # from werkzeug.security import generate_password_hash, check_password_hash
