81b537263cfd6e2ef5c8dcf1c3fae1145492527e
MatiasRamsland01 <259150@uis.no>
2021-10-27 17:59:10 +0200

    test

--- website/db.py
+++ website/db.py
@@ -66,9 +66,9 @@ class Logs(db.Model):
 class Transaction(UserMixin, db.Model):
     transaction_id = db.Column(db.Integer, primary_key=True)
     # Out Id & Money can be null because we might put in (or take out) money through an ATM
-    from_user_id = db.Column(db.Integer, nullable=True)  # TODO ForeignKey?
+    from_user_id = db.Column(db.LargeBinary, nullable=True)  # TODO ForeignKey?
     out_money = db.Column(db.Text, nullable=True)
-    to_user_id = db.Column(db.Integer)  # TODO ForeignKey?
+    to_user_id = db.Column(db.LargeBinary)  # TODO ForeignKey?
     in_money = db.Column(db.Text)
     message = db.Column(db.String(120))
 
